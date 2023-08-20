f8c8074ba3b3b33e9ba29a9c19067e0dafd8863a
githadmi <fun.auth.69g@gmail.com>
2023-08-20 22:06:18 +0530

    added redis

--- test_with_worker_service.js
+++ test_with_worker_service.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 const kafka = require("kafka-node");
+const http = require("http");
+const socketIo = require("socket.io");
+const Redis = require("ioredis");
+
+const redis = new Redis(); // Default Redis connection to localhost:6379
 
 // MongoDB connection
 mongoose
@@ -36,11 +41,9 @@ const messageLogSchema = new mongoose.Schema({
   ip: String,
 });
 
-// Create a unique index on the messageId field
+// Create a unique index on the messageId field for efficient lookups
 messageLogSchema.index({ messageId: 1 }, { unique: true });
-
 const MessageLog = mongoose.model("MessageLog", messageLogSchema);
-
 
 // Kafka Client Configuration
 const kafkaClientOptions = {
@@ -56,6 +59,34 @@ const kafkaConsumer = new kafka.Consumer(
   [{ topic: "ip-status-updates", partition: 0 }],
   { autoCommit: true }
 );
+
+// WebSocket server setup for client communication
+const server = http.createServer();
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  const redisSubscriber = new Redis();
+  redisSubscriber.subscribe("ip-status-updates");
+
+  // Forward the Redis updates to the connected client
+  redisSubscriber.on("message", (channel, message) => {
+    if (channel === "ip-status-updates") {
+      socket.emit("update", message);
+    }
+  });
+
+  // Clean up Redis connection on client disconnect
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    redisSubscriber.quit();
+  });
+});
+
+server.listen(3001, () => {
+  console.log("WebSocket server listening on port 3001");
+});
 
 // Handle incoming messages
 kafkaConsumer.on("message", (message) => {
@@ -75,43 +106,68 @@ kafkaConsumer.on("message", (message) => {
       const { ip, status, timestamp, messageId } = ipStatusUpdate;
 
       // Update the database based on the incoming message
-      if (status === "UP") {
+      // if (status === "UP") {
+      //   IPStatus.findOneAndUpdate(
+      //     { ip: ip },
+      //     {
+      //       ip: ip,
+      //       isDown: false,
+      //       lastUp: new Date(timestamp),
+      //       wasDown: false,
+      //     },
+      //     { upsert: true, new: true }
+      //   )
+      //     .then((document) => {
+      //       console.log("Database updated successfully for UP status");
+      //     })
+      //     .catch((error) => {
+      //       console.log(
+      //         "Error while updating IP status in database:",
+      //         error.message
+      //       );
+      //     });
+      // } else if (status === "DOWN") {
+      //   IPStatus.findOneAndUpdate(
+      //     { ip: ip },
+      //     {
+      //       ip: ip,
+      //       isDown: true,
+      //       lastDown: new Date(timestamp),
+      //       wasDown: true,
+      //     },
+      //     { upsert: true, new: true }
+      //   )
+      //     .then((document) => {
+      //       console.log("Database updated successfully for DOWN status");
+      //     })
+      //     .catch((error) => {
+      //       console.log(
+      //         "Error while updating IP status in database:",
+      //         error.message
+      //       );
+      //     });
+      // }
+      if (status === "UP" || status === "DOWN") {
         IPStatus.findOneAndUpdate(
           { ip: ip },
           {
             ip: ip,
-            isDown: false,
-            lastUp: new Date(timestamp),
-            wasDown: false,
+            isDown: status === "DOWN",
+            lastUp: status === "UP" ? new Date(timestamp) : undefined,
+            lastDown: status === "DOWN" ? new Date(timestamp) : undefined,
+            wasDown: status === "DOWN",
           },
           { upsert: true, new: true }
         )
           .then((document) => {
-            console.log("Database updated successfully for UP status");
+            console.log(`Database updated successfully for ${status} status`);
+
+            // Publish the status update to a Redis channel
+            redis.publish("ip-status-updates", JSON.stringify(ipStatusUpdate));
           })
           .catch((error) => {
             console.log(
-              "Error while updating IP status in database:",
-              error.message
-            );
-          });
-      } else if (status === "DOWN") {
-        IPStatus.findOneAndUpdate(
-          { ip: ip },
-          {
-            ip: ip,
-            isDown: true,
-            lastDown: new Date(timestamp),
-            wasDown: true,
-          },
-          { upsert: true, new: true }
-        )
-          .then((document) => {
-            console.log("Database updated successfully for DOWN status");
-          })
-          .catch((error) => {
-            console.log(
-              "Error while updating IP status in database:",
+              `Error while updating IP status in database:`,
               error.message
             );
           });
